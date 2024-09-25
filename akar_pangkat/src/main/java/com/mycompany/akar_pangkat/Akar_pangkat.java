8a50261b817879dcba6cf8a87fe0d1b8a60bebfb
dhavinaaa <dhavinaputrina@gmail.com>
2024-09-25 09:40:00 +0700

    menjelaskan alasan error debuging

---
+++ akar_pangkat/src/main/java/com/mycompany/akar_pangkat/Akar_pangkat.java
@@ -0,0 +1,15 @@
+
+package com.mycompany.akar_pangkat;
+
+    public class Akar_pangkat {
+
+ 
+public static void main(String [] args) 
+{ 
+int a=10; int b= 2; 
+double akar_a=Math.pow(a,2); 
+double akar3_b=Math.pow(b,3); 
+double penjumlahan_keduanya=akar_a+akar_b; 
+// 
+} 
+} 
